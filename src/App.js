4742c36a161357bee7225a3ad7098a914f5999cf
Andrés Sandoval <a.sandoval1303@uniandes.edu.co>
2019-04-04 16:14:01 -0500

    faltan 5 preguntas

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import firebase from './firebase.js';
 import { JsonToTable } from "react-json-to-table";
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar, BarChart, ResponsiveContainer
+  XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar, BarChart, ResponsiveContainer
 } from 'recharts';
 import Select from 'react-select';
 
@@ -11,25 +11,75 @@ class App extends Component {
 
   constructor(props) {
 
+    super(props);
+    this.state = {
+    dropdownValue: 0,
+    firebaseDataResults:{},
+    firebaseDataProtocols:{},
+    firebaseDataTests:{},
+    firebaseDataPatients:{},
+    shownDatabaseTable : {},
+    graphData:[]
+    };
 
-      super(props);
-      this.state = {
-      firebaseData:{},
-      graphData:[]
-            };
     this.getResultsDatabase = this.getResultsDatabase.bind(this);
+    this.getProtocolsDatabase = this.getProtocolsDatabase.bind(this);
     this.getTestsDatabase = this.getTestsDatabase.bind(this);
     this.getMostUsedProtocols = this.getMostUsedProtocols.bind(this);
     this.getTestWithMostSubtests = this.getTestWithMostSubtests.bind(this);
+    this.getTestWithMostItems= this.getTestWithMostItems.bind(this);
+    this.getProtocolWithMostTests= this.getProtocolWithMostTests.bind(this);
     this.handleDropdown = this.handleDropdown.bind(this);
 }
   componentWillMount(){
-    //this.getResultsDatabase();
+    this.getResultsDatabase();
+    this.getProtocolsDatabase();
     this.getTestsDatabase();
   }
 
-  handleDropdown(value){
+  handleDropdown(event){
+    var question = event.value;
+    var dbTable = {};
 
+    if(question === 1){
+      dbTable = this.state.firebaseDataResults;
+      this.getMostUsedProtocols();
+    } else if (question === 2) {
+      dbTable = this.state.firebaseDataResults;
+
+    } else if (question === 3) {
+      dbTable = this.state.firebaseDataResults;
+
+    } else if (question === 4) {
+      dbTable = this.state.firebaseDataResults;
+
+    } else if (question === 5) {
+      dbTable = this.state.firebaseDataResults;
+
+    } else if (question === 6) {
+      dbTable = this.state.firebaseDataResults;
+
+    } else if (question === 7) {
+      dbTable = this.state.firebaseDataProtocols;
+      this.getProtocolWithMostTests();
+
+    } else if (question === 8) {
+      dbTable = this.state.firebaseDataTests;
+      this.getTestWithMostItems();
+    } else if (question === 9) {
+      dbTable = this.state.firebaseDataTests;
+      this.getTestWithMostSubtests();
+    } else if (question === 10) {
+      dbTable = this.state.firebaseDataTests;
+      this.getTestWithMostSubtests();
+    } else{
+
+    }
+
+    this.setState({
+      dropdownValue:question,
+      shownDatabaseTable: dbTable
+      })
   }
 
   getResultsDatabase = () => {
@@ -37,7 +87,17 @@ class App extends Component {
     var data = JSON.parse(JSON.stringify(snap.val()));
 
     this.setState({
-      firebaseData:data
+      firebaseDataResults:data
+      })
+    }.bind(this));
+  }
+
+  getProtocolsDatabase = () => {
+    firebase.database().ref('protocolos/').once('value', function (snap){
+    var data = JSON.parse(JSON.stringify(snap.val()));
+
+    this.setState({
+      firebaseDataProtocols:data
       })
     }.bind(this));
   }
@@ -47,13 +107,13 @@ class App extends Component {
     var data = JSON.parse(JSON.stringify(snap.val()));
 
     this.setState({
-      firebaseData:data
+      firebaseDataTests:data
       })
     }.bind(this));
   }
 
   getMostUsedProtocols (){
-    var data = Object.values(this.state.firebaseData);
+    var data = Object.values(this.state.firebaseDataResults);
     var protocolData = {};
     var newData = [];
     var counts = data.reduce((p, c) => {
@@ -79,8 +139,7 @@ class App extends Component {
   }
 
   getTestWithMostSubtests (){
-    var data = Object.values(this.state.firebaseData);
-    console.log(data);
+    var data = Object.values(this.state.firebaseDataTests);
     var testData = {};
     var newData = [];
 
@@ -95,8 +154,55 @@ class App extends Component {
     this.setState({
       graphData:newData
     })
-}
+  }
 
+  getTestWithMostItems (){
+    var data = Object.values(this.state.firebaseDataTests);
+    var testData = {};
+    var newData = [];
+
+    for(var i = 0; i<data.length; i++){
+      var itemsPorTest = 0;
+
+        var subtests = Object.values(data[i].Bloques);
+        for(var j = 0; j<subtests.length; j++){
+          if(subtests[j].hasOwnProperty("Bloques")){
+          itemsPorTest += Object.values(subtests[j].Bloques).length;
+        }
+      }
+      testData = {
+        "id" : data[i].nombre,
+        "value" : itemsPorTest
+      };
+      newData.push(testData);
+    }
+    this.setState({
+      graphData:newData
+    })
+  }
+
+  getProtocolWithMostTests (){
+    var data = Object.values(this.state.firebaseDataProtocols);
+    console.log(data);
+    var protocolData = {};
+    var newData = [];
+
+    for(var i = 0; i<data.length; i++){
+      var numTests = 0;
+      if(data[i].hasOwnProperty("Tests")){
+        numTests += Object.values(data[i].Tests).length;
+      }
+      protocolData = {
+        "id" : data[i].nombre,
+        "value" : numTests
+      };
+      newData.push(protocolData);
+    }
+
+    this.setState({
+      graphData:newData
+    })
+  }
 
   render() {
 
@@ -109,9 +215,9 @@ class App extends Component {
       { label: "¿Cuál es el promedio de puntaje para cierto ítem?", value: 5 },
       { label: "¿Cuál es el protocolo más aplicado a examinados entre cierto rango de edad?", value: 6 },
       { label: "¿Cuál es el protocolo más largo o más corto (según el número de tests)?", value: 7 },
-      { label: "¿Qué cantidad de protocolos ha presentado cierto examinado?", value: 8 },
-      { label: "¿Cuál es el test con más o con menos ítems?", value: 9 },
-      { label: "¿Cuál es el test con más o con menos subtests?", value: 10 },
+      { label: "¿Cuál es el test con más o con menos ítems?", value: 8 },
+      { label: "¿Cuál es el test con más o con menos subtests?", value: 9 },
+      { label: "¿Cuál es el subtest con más o con menos items?", value: 10 },
     ];
 
     return (
@@ -120,14 +226,13 @@ class App extends Component {
           PsychoTest
         </div>
 
-        <Select options={ preguntasAnalitica } />
+        <Select options={ preguntasAnalitica } onChange={this.handleDropdown}/>
 
           <div className ="row mainWindow">
             <div className = "col-md-4 tablaFireBase">
-              <JsonToTable json={this.state.firebaseData} />
+              <JsonToTable json={this.state.shownDatabaseTable} />
             </div>
             <div className = "col-md-8 graficas">
-              <button onClick={this.getTestWithMostSubtests}>Get Data</button>
 
             <div style={{ width: '100%', height: '70vh' }}>
             <ResponsiveContainer>
